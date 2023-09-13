7d148e7826a0b92d85ca0597485cd3ba4fcc0ac4
albitesrodrigo <albitesrodrigo@gmail.com>
2023-09-13 09:17:06 -0500

    Se agrega el proyecto

---
+++ CL1LaboPeruSAC/Controllers/DiscoSolidoController.cs
@@ -0,0 +1,40 @@
+﻿using CL1LaboPeruSAC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CL1LaboPeruSAC.Controllers
+{
+    public class DiscoSolidoController : Controller
+    {
+        public IConfiguration _config;
+
+        //creamos un constructor...
+        public DiscoSolidoController(IConfiguration IConfig)
+        {
+            _config = IConfig;
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToAction("Index", "VentasHardware");
+            //return View();
+        }  //fin del metodo...
+
+        public IActionResult DiscoSolido()
+        {
+            return View();
+        }  //fin del metodo...
+
+        public IActionResult CalcularCds(ClassCalcular calcular)
+        {
+            int capacidadGb = calcular.Disco;
+            int capacidadMb = capacidadGb * 1024;
+            int numeroCds = (int)Math.Ceiling(capacidadMb / 700.0);
+
+            // Pasar los resultados al modelo
+            calcular.total = numeroCds;
+            //calcular.total = numeroCds * 700;
+
+            return View(calcular);
+        }
+    }
+}
---
+++ CL1LaboPeruSAC/Models/ClassCalcular.cs
@@ -0,0 +1,10 @@
+﻿namespace CL1LaboPeruSAC.Models
+{
+    public class ClassCalcular
+    {
+        public int Disco { get; set; }
+        public int Cds { get; set; }
+        public int total { get; set; }
+
+    }
+}
